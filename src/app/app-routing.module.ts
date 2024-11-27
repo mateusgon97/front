c6a404a79264a47de0381ea4b0a833c0bc416104
Mateus Oliveira <116849388+mateusgon97@users.noreply.github.com>
2024-11-26 23:22:41 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LancamentoVendasComponent } from './pages/lancamento-vendas/lancamento-vendas.component';
+import { VisualizacaoVendasComponent } from './pages/lancamento-vendas/visualizacao-vendas.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'lancamento-vendas', pathMatch: 'full' },
+  { path: 'lancamento-vendas', component: LancamentoVendasComponent },
+  { path: 'visualizacao-vendas', component: VisualizacaoVendasComponent }
 ];
 
 @NgModule({
@@ -19,4 +15,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
